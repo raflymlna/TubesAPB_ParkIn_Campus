b308d57c4b9542303698a5a1d08be0a633347f45
raflymlna <ralph.dutz@gmail.com>
2026-05-24 19:49:16 +0700

    setup firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'views/auth/login_page.dart';
 import '/core/theme/app_theme.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
