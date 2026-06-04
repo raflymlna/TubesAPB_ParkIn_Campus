acb7959889b83b4be8fdc1c5ca597472d992cd83
jadensoftspoken <ferdira7@gmail.com>
2026-06-04 16:06:16 +0700

    ganti author

--- lib/parking/find_my_ride.dart
+++ lib/parking/find_my_ride.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; // ganti author
 
 class FindMyRidePage extends StatelessWidget {
   const FindMyRidePage({super.key});
